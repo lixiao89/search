f05f0d6df5048cb9f4b85e19ae131ad896ec8c2c
Xiao Li <xli4217@gmail.com>
2015-12-23 21:47:22 -0500

    scrape ICRA table of contents

--- scrapy_project/search_test/search_test/spiders/IEEESpider.py
+++ scrapy_project/search_test/search_test/spiders/IEEESpider.py
@@ -6,6 +6,10 @@ class IEEESpider(scrapy.Spider):
     start_urls = ["http://ieeexplore.ieee.org/xpl/mostRecentIssue.jsp?punumber=7128761"]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2] + '.html'
+        # filename = response.url.split("/")[-2] + '.html'
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body) 
+
+        filename = "titles.txt"
         with open(filename, 'wb') as f:
-            f.write(response.body)
+                f.write(response.xpath('//h3/a/span/@id/text()').extract()) 
