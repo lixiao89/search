238ff5bbcf90cd18d4cd39f23bd328b4964c82f1
me <me>
2015-12-26 12:19:28 -0500

    update before working on multiple start_urls scraping, problem with callback execution time, can't write down everything in time

--- scrape/scrapy_test/search_test/spiders/IEEESpider.py
+++ scrape/scrapy_test/search_test/spiders/IEEESpider.py
@@ -1,11 +1,13 @@
 import scrapy
 from selenium import webdriver
 import time
+import os.path
 
 class IEEESpider(scrapy.Spider):
     name = "IEEE"
     allowed_domains = ["ieeexplore.ieee.org"]
-    start_urls = ["http://ieeexplore.ieee.org/xpl/mostRecentIssue.jsp?punumber=7128761"]
+    start_urls = ["http://ieeexplore.ieee.org/xpl/mostRecentIssue.jsp?punumber=7128761"]#,
+    #"http://ieeexplore.ieee.org/xpl/mostRecentIssue.jsp?punumber=7347169"]
 
     def __init__(self):
         path_to_chromedriver = '/Users/xiaoli/Desktop/search/scrape/selenium_test/chromedriver'
@@ -14,9 +16,18 @@ class IEEESpider(scrapy.Spider):
 
         self.filename = "titles.txt"
         self.title_count = 1
-        
+
+        if os.path.isfile(self.filename):
+            os.remove(self.filename)
+
+        #self.browser.find_element_by_xpath('//*[@id="rows-per-page"]/option[contains(text(),"100")]').click()
+            
+            
     def parse(self, response):
 
+        if self.title_count == 1:
+            self.parse_dir_contents(response)
+            
         while True:
             try:
                 next = self.browser.find_element_by_xpath('//*[@id="results-blk"]/div[2]/div/a[7]')
@@ -24,18 +35,17 @@ class IEEESpider(scrapy.Spider):
                 time.sleep(5)
                 yield scrapy.Request( self.browser.current_url, callback=self.parse_dir_contents)
             except:
-                f.close()
                 break
         
 
     def parse_dir_contents(self, response):
 
         titles = response.xpath('//h3/a/span/text()').extract() 
-        with open(self.filename, 'ab') as f:
+        with open(self.filename, 'a') as f:
             for title in titles:
                 f.write(str(self.title_count) + '. ')
                 f.write(title + '\n')
-                f.write('\n')
+                f.write("\n")
                 self.title_count += 1
         
                     
